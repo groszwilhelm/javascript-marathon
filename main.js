907e1980b4ff4d13b2207d8b9e12f8c6cc052958
groszwi <groszwi@bisnode.com>
2022-10-19 18:43:36 +0300

    adds initial changes

---
+++ main.js
@@ -0,0 +1,45 @@
+/**
+ * JavaScript
+ * 
+ * 
+ * Primitive data types
+ * - String
+ * - Number (Integer, Float)
+ * - Boolean (true or false)
+ * - undefined (Set by javascript on non existing data)
+ * - null (Set by us when we don't have data)
+ * 
+ * - Symbol
+ * - Bigint
+ * 
+ * Complex data types
+ * - Object (Dictionary)
+ * - Array (List)
+ * 
+ */
+
+// Variables -> reusability, store information 
+// const > let > var
+
+/**
+ * Const prevents us from reassigning the variable
+ */
+const hello = 'World';
+
+let hello2 = 'World';
+
+hello2 = 'Hello Levi';
+
+// To be avoided even though it works
+something = 'Hello';
+
+const num = 123;
+
+const alwaysRight = true;
+
+const noValue = null;
+
+// modulo
+const result = 2 % 4;
+
+console.log(result);
